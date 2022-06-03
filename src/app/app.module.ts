8addb76dfa07ed6d84d3772d61c320a3b6f42b10
Waldyr Felix <waldyr@rccnet.com.br>
2022-06-03 14:49:17 -0300

    Adicionadas rotas do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,9 +12,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TeamsComponent } from './teams/teams.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, TeamsComponent],
+  declarations: [AppComponent, TeamsComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
